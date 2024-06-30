f485f63242cdd31986b6afaa460dd6b803e26fa0
Falco13 <keanu0608@gmail.com>
2024-06-30 14:15:28 +0300

    added views and routes

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('title') About @endsection
+@section('main_content')
+
+<h1>Page about</h1>
+
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('title') Contact @endsection
+@section('main_content')
+
+<h1>Page Contact in developing now...</h1>
+
+@endsection
